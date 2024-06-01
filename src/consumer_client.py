a95b660e02e3ca5ee939a10e3d622ee5cac45a44
adityapentyala <adityapentyala02@gmail.com>
2024-06-01 10:22:25 +0530

    docs: add docstrings

--- src/consumer_client.py
+++ src/consumer_client.py
@@ -1,3 +1,7 @@
+"""
+Consumer script that receives data as bytes from Kafka stream, converts back to json, and POSTs requests to 
+add json data to a db using Gilhari API
+"""
 from kafka import KafkaConsumer
 import json
 import requests
@@ -5,13 +9,42 @@ import requests
 BASE_URL = "http://localhost:80/gilhari/v1"
 
 def json_deserializer(data):
+    """
+    Converts bytes data from stream back to json
+
+    Args:
+        data: bytes - consumer message value to be converted to json
+    
+    returns:
+        dict
+    """
     return json.loads(data.decode('utf-8'))
     
 def insert_json_data(endpoint, json=None, headers=None, data=None):
+    """
+    Creates POST request to API endpoint to add new entity to database
+
+    Args:
+        json: dict[str, dict[str, Any]] - entity to be added to the db. Defaults to None.
+        headers: _HeadersMapping - placeholder argument. Defaults to None.
+        data: _Data - placeholder argument. Defaults to None.
+    
+    returns:
+        Any
+    """
     response = requests.post(f'{BASE_URL}/{endpoint}/', data=data, json=json, headers=headers)
     return handle_response(response)
 
 def handle_response(response):
+    """
+    Handles response received from API request
+
+    Args:
+        response: Response - response received from the API
+    
+    returns:
+        Any
+    """
     if response.status_code >= 200 and response.status_code < 300:
         try:
             return response.json()
@@ -30,6 +63,15 @@ consumer = KafkaConsumer(
 )
 
 def consume_stream(uptime):
+    """
+    Creates a KafkaConsumer to listen to the Kafka stream and adds data to the db using the Gihari API
+
+    Args:
+        uptime: int - Time in seconds for which consumer should remain active
+    
+    returns:
+        None
+    """
     consumer = KafkaConsumer(
         'Employees',
         bootstrap_servers=['localhost:9092'],
--- src/producer.py
+++ src/producer.py
@@ -1,9 +1,21 @@
+"""
+Producer script that sends json data to a Kafka stream for the consumer client to receive
+"""
 from kafka import KafkaProducer
 import json
 import time
 import random
 
 def json_serializer(data):
+    """
+    Function to serialize and encode json data as binary
+
+    Args:
+        data: dict[str, Any] - json data to be encoded
+    
+    returns:
+        bytes
+    """
     return json.dumps(data).encode('utf-8')
 
 producer = KafkaProducer(
@@ -12,6 +24,16 @@ producer = KafkaProducer(
 )
 
 def produce_stream(up_time):
+    """
+    Function that uses previously defined KafkaProducer to send data to the streaming service. Function
+    takes one argument to generate data for a set number of seconds, sending a datapoint every 3-7 seconds
+
+    Args:
+        up_time: int - time in seconds during which producer must send data
+
+    returns:
+        None
+    """
     count=2
     time.sleep(4)
     start_time = time.time()
