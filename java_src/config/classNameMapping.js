42b8f659b7f8a271756c684da77d9c5769c11add
adityapentyala <adityapentyala02@gmail.com>
2024-06-02 22:40:42 +0530

    feat: change schema for sales and inventory

--- java_src/config/classNameMapping.js
+++ java_src/config/classNameMapping.js
@@ -1,1 +1,3 @@
-{"Employee":"java_src.src.main.java.models.JSON_Employee"}
+{"Employees":"java_src.src.main.java.models.JSON_Employee",
+ "Inventory":"java_src.src.main.java.models.JSON_InventoryItem",
+ "Sales":"java_src.src.main.java.models.JSON_Sale"}
